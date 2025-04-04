e559e94296dda9bb4c5a2f3b9b6df61526361e26
Hałas Piotr <halaspiotr@gmail.com>
2025-04-05 01:30:40 +0200

    Add esi demo

---
+++ presentations/01-esi/demos/esi/www/_component/header.php
@@ -0,0 +1,5 @@
+<?php
+if (!headers_sent()) {
+    header("Cache-Control: public, max-age=10");
+}
+usleep(100000); echo "Header";
---
+++ presentations/01-esi/demos/esi/www/_component/left_menu.php
@@ -0,0 +1,9 @@
+<?php
+if (!headers_sent()) {
+    header("Cache-Control: public, max-age=10");
+}
+usleep(100000);?>
+Left Menu
+
+<p><a href="/">Standard</a></p>
+<p><a href="/esi.php">ESI</a></p>
